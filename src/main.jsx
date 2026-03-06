9d6edbffa8c5ccacf5edcd0cca8506b3fe7f5db8
corytimmons1688 <ctimmons@calyxcontainers.com>
2026-03-06 12:39:12 -0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-ReactDOM.createRoot(document.getElementById("root")).render(<React.StrictMode><App/></React.StrictMode>);
+
+class ErrorBoundary extends React.Component {
+  constructor(props) { super(props); this.state = { err: null, info: null }; }
+  componentDidCatch(err, info) { this.setState({ err, info }); }
+  render() {
+    if (this.state.err) {
+      return React.createElement("div", {
+        style: { padding: 32, fontFamily: "monospace", background: "#1e1e2e", color: "#f38ba8", minHeight: "100vh" }
+      },
+        React.createElement("h2", null, "💥 Runtime Error"),
+        React.createElement("pre", { style: { whiteSpace: "pre-wrap", color: "#cdd6f4", background: "#181825", padding: 16, borderRadius: 8 } },
+          String(this.state.err) + "\n\n" + (this.state.info && this.state.info.componentStack ? this.state.info.componentStack : "")
+        )
+      );
+    }
+    return this.props.children;
+  }
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  React.createElement(ErrorBoundary, null, React.createElement(App, null))
+);
